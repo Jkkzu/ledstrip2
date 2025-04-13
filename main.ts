c2316de1d174c9b42bd575d6aa5582e3bd9f051d
Jkkzu <158163958+Jkkzu@users.noreply.github.com>
2025-04-13 19:35:28 +0900

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,7 +1,56 @@
-let t = 0
 let strip: neopixel.Strip = null
-let range = 0
+let t = 0
+let range: neopixel.Strip = null
 basic.forever(function () {
     strip = neopixel.create(DigitalPin.P0, 60, NeoPixelMode.RGB)
-    t = 110
+    t = 50
+    for (let index = 0; index < 4; index++) {
+        t += -10
+        for (let n = 0; n <= 60; n++) {
+            range = strip.range(0, n)
+            range.showColor(neopixel.colors(NeoPixelColors.Red))
+            basic.pause(t)
+            strip.clear()
+        }
+    }
+    t = 50
+    for (let index = 0; index < 4; index++) {
+        t += -10
+        for (let n = 0; n <= 60; n++) {
+            range = strip.range(0, n)
+            range.showColor(neopixel.colors(NeoPixelColors.Green))
+            basic.pause(t)
+            strip.clear()
+        }
+    }
+    t = 50
+    for (let index = 0; index < 4; index++) {
+        t += -10
+        for (let n = 0; n <= 60; n++) {
+            range = strip.range(0, n)
+            range.showColor(neopixel.colors(NeoPixelColors.Blue))
+            basic.pause(t)
+            strip.clear()
+        }
+    }
+    t = 50
+    for (let index = 0; index < 4; index++) {
+        t += -10
+        for (let n = 0; n <= 60; n++) {
+            range = strip.range(0, n)
+            range.showColor(neopixel.colors(NeoPixelColors.Yellow))
+            basic.pause(t)
+            strip.clear()
+        }
+    }
+    t = 50
+    for (let index = 0; index < 4; index++) {
+        t += -10
+        for (let n = 0; n <= 60; n++) {
+            range = strip.range(0, n)
+            range.showColor(neopixel.colors(NeoPixelColors.Orange))
+            basic.pause(t)
+            strip.clear()
+        }
+    }
 })
